300ef2f102dd667f35d8084188275a94f7541420
Mustafa Aytemür <maytemur@gmail.com>
2023-03-12 00:56:38 +0300

    30-FirebaseAdmobProject kotlin temelleri ilk commit

---
+++ app/src/main/java/com/maytemur/firebaseadmobproject/MainActivity.kt
@@ -0,0 +1,100 @@
+package com.maytemur.firebaseadmobproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+
+class MainActivity : AppCompatActivity() {
+    lateinit var mAdView: AdView
+    private var mInterstitialAd: InterstitialAd? = null
+    private final var TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this) {
+
+        }
+        //App-ID -> ca-app-pub-4313708117464817~2694369578
+        //banner ID- ca-app-pub-4313708117464817/6677722900
+        //InterstitialAd ID-> ca-app-pub-4313708117464817/9443896163
+
+        //banner TestID-> ca-app-pub-3940256099942544/6300978111
+        //InterstitialBanner TestId-> ca-app-pub-3940256099942544/1033173712
+
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+
+        mAdView.adListener = object : AdListener() {
+            override fun onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+            }
+
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                // Code to be executed when an ad request fails.
+            }
+
+            override fun onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+
+            override fun onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+            }
+
+            override fun onAdClosed() {
+                // Code to be executed when the user is about to return
+                // to the app after tapping on an ad.
+            }
+        }
+        InterstitialAd.load(
+            this,
+            "ca-app-pub-4313708117464817/9443896163",
+            adRequest,
+            object : InterstitialAdLoadCallback() {
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    Log.d(TAG, adError?.message)
+                    mInterstitialAd = null
+                }
+
+                override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                    Log.d(TAG, "Ad was loaded.")
+                    mInterstitialAd = interstitialAd
+                }
+            })
+
+    }
+
+    //Deneme amaçlı başka sayfa oluşturulmadı- Başka sayfa oluşturup oradada yapılabilir
+    fun gec(view: View) {
+        //The FullScreenContentCallback handles events related to displaying your InterstitialAd.
+        // Before showing InterstitialAd, make sure to set the callback
+        mInterstitialAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
+            override fun onAdDismissedFullScreenContent() {
+                Log.d(TAG, "Ad was dismissed.")
+            }
+
+            override fun onAdFailedToShowFullScreenContent(adError: AdError?) {
+                Log.d(TAG, "Ad failed to show.")
+            }
+
+            override fun onAdShowedFullScreenContent() {
+                Log.d(TAG, "Ad showed fullscreen content.")
+                mInterstitialAd = null
+            }
+        }
+        if (mInterstitialAd != null) {
+            mInterstitialAd?.show(this)
+        } else {
+            Log.d("TAG", "The interstitial ad wasn't ready yet.")
+        }
+        //Add Event kısmı banner ile aynı kullanılabilir
+    }
+}
